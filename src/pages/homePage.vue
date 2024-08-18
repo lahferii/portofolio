6a71b21dc3aef7a8c74c84c4a233f6f33743cdcc
lahferii <ferinaise@gmail.com>
2024-08-18 22:57:05 +0800

    adjust position of two section in homePage

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,7 +1,7 @@
 <script></script>
 
 <template>
-  <section class="max-w-5xl m-auto h-min-screen flex flex-col sm:flex-row items-center px-5 pt-20 sm:pt-0">
+  <section class="max-w-5xl m-auto h-screen flex flex-col sm:flex-row items-center px-5 pt-20 sm:pt-0">
     <div class="sm:flex-1">
       <img class="w-4/6 sm:w-3/4 m-auto object-fit object-center" src="../assets/hero.png" alt="hero.png">
     </div>
