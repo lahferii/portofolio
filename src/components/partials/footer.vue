4daa85d2e28cadca0acb0cb961ae80670c5e66c2
lahferii <ferinaise@gmail.com>
2024-08-18 23:00:38 +0800

    adjust position of two section in homePage

--- src/components/partials/footer.vue
+++ src/components/partials/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pb-2 pt-10 text-center">
+  <div class="pb-2 text-center">
     <p class="tracking-widest text-sm">Crafted with ❤ by <span class="font-semibold">Peri Nurjaman</span></p>
   </div>
 </template>
