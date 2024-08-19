93741485e06ffc4f8ffbfa1e58060ae0ce41890c
lahferii <ferinaise@gmail.com>
2024-08-19 23:05:02 +0800

    add projects page

--- src/pages/projectPage.vue
+++ src/pages/projectPage.vue
@@ -1,3 +1,78 @@
 <template>
-  <h1>Project Page</h1>
+  <section class="wrapper">
+    <div>
+      <div class="md:w-2/3">
+        <h1 class="text-2xl font-semibold tracking-wider mb-4">Latest Projects</h1>
+        <p class="tracking-widest leading-relaxed mb-2">
+            Honestly, I rarely create a whole complete project. Instead, I tend to concentrate on developing small, separate features indepedently.
+        </p>
+        <p class="tracking-widest leading-relaxed">
+          However, here are a few projects that I have fully completed so far.
+        </p>
+      </div>
+    
+      <div class="flex py-5 w-full h-72 md:h-auto gap-5 flex-col md:flex-row overflow-y-scroll md:overflow-y-hidden">
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h3 class="text-xl font-semibold tracking-wider mb-2">Tokoku</h3>
+            <p class="text-sm tracking-wider underline">
+              See Project 
+              <i class="fas fa-arrow-right"></i>
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h3 class="text-xl font-semibold tracking-wider mb-2">Movinfo</h3>
+            <p class="text-sm tracking-wider underline">
+              See Project 
+              <i class="fas fa-arrow-right"></i>
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h3 class="text-xl font-semibold tracking-wider mb-2">TKJ Absurd</h3>
+            <p class="text-sm tracking-wider underline">
+              See Project 
+              <i class="fas fa-arrow-right"></i>
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h3 class="text-xl font-semibold tracking-wider mb-2">Shayna Store</h3>
+            <p class="text-sm tracking-wider underline">
+              See Project 
+              <i class="fas fa-arrow-right"></i>
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h3 class="text-xl font-semibold tracking-wider mb-2">Selekda Esport Indonesia</h3>
+            <p class="text-sm tracking-wider underline">
+              See Project 
+              <i class="fas fa-arrow-right"></i>
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h3 class="text-xl font-semibold tracking-wider mb-2">TKJ Lootbox</h3>
+            <p class="text-sm tracking-wider underline">
+              See Project 
+              <i class="fa fa-arrow-right"></i>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
