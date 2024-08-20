8de7972fbd4c5ff4b7cca8491616038e1b2ddbd7
lahferii <ferinaise@gmail.com>
2024-08-21 04:41:13 +0800

    add track record section

--- src/pages/aboutPage.vue
+++ src/pages/aboutPage.vue
@@ -10,16 +10,69 @@
         </p>
       </div>
     </div>
-    <div>
+
+    <div class="mb-10">
       <div class="w-full h-80 overflow-hidden rounded relative group">
         <img src="../assets/about.png" alt="about.jpg" class="w-full h-full object-cover object-center">
         
         <article class="absolute z-10 w-full h-full top-0 group-hover:bg-gradient-to-b group-hover:from-transparent group-hover:to-black cursor-pointer">
-          <h3 class="hidden group-hover:block text-xl md:text-3xl font-semibold tracking-widest bottom-5 left-5 absolute">
+          <h3 class="text-gray-100 hidden group-hover:block text-xl md:text-3xl font-semibold tracking-widest bottom-5 left-5 absolute">
             Lomba Keterampilan Siswa Nasional 2022
           </h3>
         </article>
       </div>
     </div>
+
+    <article>
+      <p class="tracking-wider mb-5 leading-relaxed">
+        Hello, my name is Peri Nurjaman, a student with strong passion in the field of <span class="font-semibold">Web Development and Programming stuff</span>. I started my journey by learning <span class="font-semibold">PHP Programming Language</span> through videos on YouTube in 2022, before finally deciding to switch to <span class="font-semibold">JavaScript completely</span>.
+      </p>
+      <p class="tracking-wider mb-5 leading-relaxed">
+        After being rejected as an informatics student, my dream of becoming a website developer didn't just collapse. I decided not to give up and started learning <span class="font-semibold">NodeJS</span> through a <span class="font-semibold">Programmer Zaman Now</span> Courses recently. Even though I haven't finished it yet because of time constraints, I really enjoy everything I've gone through so far!
+      </p>
+      <p class="tracking-wider mb-5 leading-relaxed">
+        My interest in <span class="font-semibold">EEPIS News And Network Team (ENT)</span> didn`t come with no reason. With all the basic knowledge that I have, I believe that my abilities and skills will improve with ENT. I hope that I can implement the abilities I have and not just create dummy projects like I have done before.
+      </p>
+      <p class="tracking-wider mb-5 leading-relaxed">
+        To be honest, I'm not a great programmer. I am a programmer with a ton of <s>failure</s> experiences. However, I would love to share my journey so far!
+      </p>
+    </article>
+
+    <div class="mb-5">
+      <h1 class="text-4xl font-semibold tracking-wider mb-4">Track Record</h1>
+    </div>
+
+    <section class="mb-5">
+      <div class="record-box">
+        <p class="text-xl tracking-wider font-semibold mb-2">
+          <span class="text-primary dark:text-primary-dark font-bold">15th</span> Place In Lomba Keterampilan Siswa Nasional
+        </p>
+        <span class="text-sm tracking-widest">Pusat Prestasi Nasional</span>
+      </div>
+      <div class="record-box">
+        <p class="text-xl tracking-wider font-semibold mb-2">
+          <span class="text-primary dark:text-primary-dark font-bold">2nd</span> Place In Seleksi Daerah For Asean Skill Competition (Regional Scale)
+        </p>
+        <span class="text-sm tracking-widest">Kementerian Ketenagakerjaan Republik Indonesia</span>
+      </div>
+      <div class="record-box">
+        <p class="text-xl tracking-wider font-semibold mb-2">
+          <span class="text-primary dark:text-primary-dark font-bold">11th</span> Place In Seleksi Daerah For Asean Skill Competition (National Scale)
+        </p>
+        <span class="text-sm tracking-widest">Kementerian Ketenagakerjaan Republik Indonesia</span>
+      </div>
+      <div class="record-box">
+        <p class="text-xl tracking-wider font-semibold mb-2 mb-2">
+          <span class="text-primary dark:text-primary-dark font-bold">1st</span> Place In Online Web Design Competition (OWDC)
+        </p>
+        <span class="text-sm tracking-widest">Universitas Teknologi Digital, Yogyakarta</span>
+      </div>
+      <div class="record-box">
+        <p class="text-xl tracking-wider font-semibold mb-2 mb-2">
+          <span class="text-primary dark:text-primary-dark font-bold">1st</span> Place In BYTECOMP - Web Design
+        </p>
+        <span class="text-sm tracking-widest">Universitas Lambung Mangkurat, Banjarbaru</span>
+      </div>
+    </section>
   </div>
 </template>
